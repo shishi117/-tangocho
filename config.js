edf0d3f1ff009ba6405d2ad19c2136d63aa443d9
shishi117 <112799885+shishi117@users.noreply.github.com>
2026-08-15 06:25:26 +0000

    FIX:Fix-Config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // このファイルを config.js にコピーし、Firebaseコンソール > プロジェクト設定 > マイアプリ の値を記入する。
 // ここに入る値はFirebaseの公開設定でありシークレットではない。実データ保護は firestore.rules が担う。
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBvtxD_Jf1P-BD_d3hWXmYxReu4YUdoWDA",
   authDomain: "tango-b90fd.firebaseapp.com",
   projectId: "tango-b90fd",
